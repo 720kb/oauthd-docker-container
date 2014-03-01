8b05cba13d64028069a1c54e3ac78f062fd78476
wouldgo <wouldgo84@gmail.com>
2014-03-01 02:11:33 +0100

    configuration during building phase

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-docker build -t wouldgo/oauthd . #-rm=true
+echo "Give me the domain name..."
+read DOMAIN_NAME
+
+sed -i -re"s/%DOMAIN_NAME%/$DOMAIN_NAME/" Dockerfile
+
+docker build -rm=true -t wouldgo/oauthd .
 
